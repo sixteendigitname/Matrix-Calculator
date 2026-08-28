b3f2b3ba567a182313cf4276c42693302e37df0d
Tanav Sinha <72978538+sixteendigitname@users.noreply.github.com>
2026-08-28 16:16:40 +0530

    Add files via upload
    Completed project. Addition, subtraction, matrix equations, REF, and RREF all functioning.

--- interface.py
+++ interface.py
@@ -1,72 +1,92 @@
 
 import matrixfunctions
-def fillRow(row):
-            matrix = []
-            while True:
-                entry = input("Enter a number: ")
-                if entry == "":
-                    print("Nothing entered.")
-                    continue
-                elif entry.startswith("-"):
-                    modentry = entry[1:]
-                    a = entry[1:].split(".")
-                else:
-                    modentry = entry
-                    a = entry.split(".")
-                if modentry.isdigit() or (all(x.isdigit()
-                                            for x in a) and len(a) == 2):
-                    row.append(float(entry))
-                elif entry.lower() == 'row':
-                    if row == []:
-                        print("Row cannot be empty. Please try again.")
-                    elif len(matrix) >= 1 and len(row) == len(matrix[len(matrix)-1]):
-                        matrix.append(row)
-                    elif len(matrix) == 0:
-                         matrix.append(row)
-                    else:
-                        print("Invalid row length. Please input your row again.")
-                        continue
-                    newRow = []
-                    fillRow(newRow)
-                elif entry.lower() == 'end':
-                    matrix.append(row)
-                    return matrix
-                    break
-                elif entry.lower() == 'end' and len(matrix[0]) == 0:
-                    print("Invalid entry, empty matrix.")
-                else:
-                     print("Invalid entry, integers, floats or appropriate keywords only.")
-                
+
+def enterMatrix():
+    matrix = []
+    columnsize = int(input("Enter no. of columns of matrix (integers only): "))
+    while True:
+        print("Please enter a row, with all numbers seperated by commas. Enter 'end' once the matrix is finished.")
+        unrefinedRow = input("")
+        listedRow = unrefinedRow.strip().split(",")
+        if unrefinedRow.strip() == 'end':
+            matrixfunctions.printMatrix(matrix)
+            return matrix
+            break
+        elif all(is_number(x) for x in listedRow):
+            listedRow = [float(x) for x in listedRow]
+            if len(listedRow) == columnsize:
+                matrix.append(listedRow)
+                matrixfunctions.printMatrix(matrix)
+            else:
+                print("Incorrect no. of columns, please try again.")
+            continue
+        else:
+            print("Invalid input")
+
+def is_number(string_to_check):
+    try:
+        float(string_to_check)
+        return True
+    except ValueError:
+        return False
+    
 
 def main():
-    print("Welcome to the Matrix Calculator. In order to build your matrix, enter a number.")
-    print("This will create a row in the matrice. Any number enterred after this one will be a part of this row, until you enter 'row' to create a new row" \
-    "Enter 2 matrices for addition and substraction." \
-    "Enter 1 matrice for row reduction."
-    )
-    print("Enter 'end' to finish your matrix.")
-    matricesList = []
-    matrix1 = fillRow([])
-    matricesList.append(matrix1)
-    while True:
-         moreMatrices = input("Would you like to input another matrice? y/n: ")
-         if moreMatrices.strip() == "y":
-            matrix1 = fillRow([])
-         elif moreMatrices.strip() == "n":
-              while True:
-                    if len(matricesList) == 2:
-                        addOrSubstract = input("Enter 'add' to add 2 matrices. Enter 'substract' to substract a matrice from another.")
-                        matrix1 = matricesList[0]
-                        matrix2 = matricesList[1]
-                        if addOrSubstract == 'add':
-                            print(matrixfunctions.matrixAddition(matrix1, matrix2))
-                        elif addOrSubstract == 'substract':
-                            print(matrixfunctions.matrixSubstraction(matrix1, matrix2))
-                        break
-                    elif len(matricesList) == 1:
-                        print(matrixfunctions.createEchelonForm(matricesList[0]))
-                        break
-         else:
-              print("Invalid input. Please try again.")
+    
+    print("Welcome to the MatrixCalculator.")
+    print("Enter '+' to add, '-' to subtract, 'REF' to get echelon form, and 'RREF' to get reduced row echelon form.")
+    print("Enter 'AB' to solve a matrix equation of the form Ax=B.")
+    operation = input("Enter operation symbol: ")
+    if operation.strip() == '+':
+        matrixAddList = []
+        print("Enter 'cont' to input another matrix after the first 2. Enter 'end' to perform the addition operation.")
+        matrix1 = enterMatrix()
+        matrix2 = enterMatrix()
+        while True:
+            check = input("")
+            matrixAddList.append(matrix1)
+            matrixAddList.append(matrix2)
+            if check.strip() == 'cont':
+                newMatrix = enterMatrix()
+                matrixAddList.append(newMatrix)
+            elif check.strip() == 'end':
+                matrixfunctions.printMatrix(matrixfunctions.matrixAddition(*matrixAddList))
+                break
+            else:
+                print("Invalid input, please try again.")
+                continue
+    elif operation.strip() == '-':
+        matrixSubtractList = []
+        print("Enter 'cont' to input another matrix after the first 2. Enter 'end' to perform the subtraction operation.")
+        matrix1 = enterMatrix()
+        matrix2 = enterMatrix()
+        while True:
+            check = input("")
+            matrixAddList.append(matrix1)
+            matrixAddList.append(matrix2)
+            if check.strip() == 'cont':
+                newMatrix = enterMatrix()
+                matrixAddList.append(newMatrix)
+            elif check.strip() == 'end':
+                matrixfunctions.printMatrix(matrixfunctions.matrixSubtraction(*matrixSubtractList))
+                break
+            else:
+                print("Invalid input, please try again.")
+                continue
+    elif operation.strip().lower() == "ref":
+        matrix = enterMatrix()
+        matrixfunctions.createEchelonForm(matrix)
+        matrixfunctions.printMatrix(matrix)
+    elif operation.strip().lower() == "rref":
+        matrix = enterMatrix()
+        matrixfunctions.createRREF(matrix)
+        matrixfunctions.printMatrix(matrix)
+    elif operation.strip().lower() == "ab":
+        print("Please enter A and B in order.")
+        a = enterMatrix()
+        b = enterMatrix()
+        matrixfunctions.solveMatrixEquation(a, b)
+
 main()
 
+            
--- matrixfunctions.py
+++ matrixfunctions.py
@@ -1,16 +1,33 @@
-def matrixAddition(matrix1, matrix2):
-    if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):  
-        matrix3 = [[] for i in range(len(matrix1))]
-        for i in range(len(matrix1)):
-            for a in range(len(matrix1[i])):
-                matrix3[i].append(matrix1[i][a] + matrix2[i][a])
-        return matrix3
+def matrixAddition(*matrices):
+    row_counts = {len(matrix) for matrix in matrices}
+    col_counts = {len(row) for matrix in matrices for row in matrix}
+    if len(row_counts) <= 1 and  len(col_counts) <= 1:
+        matrix = [[0 for j in range(len(matrices[0][0]))] for i in range(len(matrices[0]))]
+        for i in range(len(matrices[0])):
+            for a in range(len(matrices[0][i])):
+                positionSum = sum(m[i][a] for m in matrices)
+                matrix[i][a] = positionSum
+        return matrix
     else:
         print("Invalid matrices. Matrices must be of the same dimensions.")
 
-def matrixSubstraction(matrix1, matrix2):
+def matrixSubtraction(*matrices):
+    row_counts = {len(matrix) for matrix in matrices}
+    col_counts = {len(row) for matrix in matrices for row in matrix}
+    if len(row_counts) <= 1 and  len(col_counts) <= 1:
+        matrix = [[0 for j in range(len(matrices[0][0]))] for i in range(len(matrices[0]))]
+        for i in range(len(matrices[0])):
+            for a in range(len(matrices[0][i])):
+                positionSum = sum(m[i][a] for m in matrices[1:])
+                positionDifference = matrices[0][i][a] - positionSum
+                matrix[i][a] = positionDifference
+        return matrix
+    else:
+        print("Invalid matrices. Matrices must be of the same dimensions.")
+
+def matrixSubtraction(matrix1, matrix2):
     if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):  
-        matrix3 = [[] for i in range(len(matrix1))]
+        matrix3 = [[0] for i in range(len(matrix1))]
         for i in range(len(matrix1)):
             for a in range(len(matrix1[i])):
                 matrix3[i].append(matrix1[i][a] - matrix2[i][a])
@@ -21,37 +38,115 @@ def matrixSubstraction(matrix1, matrix2):
 #Elementary row operations: Replace, + kx, kx
 
 def createEchelonForm(matrix):
+    pivotColList = []
     column = 0
     pivotRows = 0
-    while pivotRows < len(matrix):
-        while column < len(matrix[0]):
-            truthVar = scanColumn(matrix, pivotRows, column)
-            if truthVar or truthVar == 0:
-                pivotRows += 1
-                placeholder1 = matrix[truthVar]
-                placeholder2 = matrix[pivotRows]
-                matrix[truthVar] = placeholder2
-                matrix[pivotRows] = placeholder1
-            column+=1
+    while pivotRows < len(matrix) and column < len(matrix[0]):
+        truthVar = scanColumn(matrix, pivotRows, column)[0]
+        if truthVar or truthVar == 0:
+            placeholder1 = matrix[truthVar]
+            placeholder2 = matrix[pivotRows]
+            matrix[truthVar] = placeholder2
+            matrix[pivotRows] = placeholder1
+            eliminatBelowRows(matrix, pivotRows, column)
+            pivotColList.append(((pivotRows, column)))
+            pivotRows += 1
+        column+=1
         continue
-            
+    return pivotColList
+
+def createRREF(matrix):
+    pivotColList = createEchelonForm(matrix)
+    for (row, column) in pivotColList:
+        scalePivotRow(matrix, row, column)
+        eliminateAboveRows(matrix, row, column)
+    return matrix, pivotColList
+    
+
+def eliminatBelowRows(matrix, pivotRows, column):
+    p = matrix[pivotRows][column]
+    for someRow in range(pivotRows + 1, len(matrix)):
+        v = matrix[someRow][column]
+        if v == 0:
+           continue
+        else:
+            k = v/p
+            for col in range(len(matrix[0])):
+                matrix[someRow][col] = matrix[someRow][col] - k * matrix[pivotRows][col]
+                matrix[someRow][col] = round(matrix[someRow][col], 10)
+
+def eliminateAboveRows(matrix, pivotRows, column):
+    for someRow in range(pivotRows):
+        v = matrix[someRow][column]
+        if v == 0:
+            continue
+        else:
+            for col in range(len(matrix[0])):
+                matrix[someRow][col] = matrix[someRow][col] - v*matrix[pivotRows][col]
+                matrix[someRow][col] = round(matrix[someRow][col], 10)
 
 def scanColumn(matrix, row, column):
     if matrix[row][column] == 0 and row < len(matrix) - 1:
         return scanColumn(matrix, row+1, column)
     elif matrix[row][column] != 0:
-        return row
+        return (row, column)
     elif row == len(matrix) - 1:
         if matrix[row][column] != 0:
-            return row
+            return (row, column)
         else:
-            return None
+            return None, None
 
+def scalePivotRow(matrix, pivotRows, column):
+    p = matrix[pivotRows][column]
+    for col in range(len(matrix[0])):
+        matrix[pivotRows][col] = matrix[pivotRows][col] / p
+        matrix[pivotRows][col] = round(matrix[pivotRows][col], 10)
 
+def printMatrix(matrix):
+    for row in matrix:
+        print(row)
 
+def concatenate(matrix1, matrix2):
+    if len(matrix1)==len(matrix2):
+        result = [[] for i in range(len(matrix1))]
+        for i in range(len(matrix1)):
+            result[i] = matrix1[i] + matrix2[i]
+        return result
+    else:
+        print("Both matrices need the same number of rows, please try again.")
 
-test1 = [[1.0,2.0,3.0], [0.0,2.0,3.0], [1.0,2.0,3.0]]
-test2 = [[2.0,3.0,4.0], [5.0,6.0,7.0], [8.0,9.0,10.0]]
-zeroMatrix = [[0 for j in range(len(test1[0]))] for i in range(len(test1))]
+def solveMatrixEquation(A, B):
+    augmented, pivotColList = createRREF(concatenate(A, B))
+    identityMatrixCheck = all(x==y for (x,y) in pivotColList) and len(pivotColList)==len(A)
+    solutionsDict = {}
+    for col in range(len(A[0]), len(augmented[0])):
+        noSolutionCheck = False
+        for row in range(len(A)):
+            if augmented[row][col] != 0 and all(x==0 for x in augmented[row][:len(A)]):
+                    if len(B[0]) > 1:
+                        print(f"No solution for A with column {col} of B.")
+                    else:
+                        print("No solution")
+                    noSolutionCheck = True
+                    solutionsDict[col-len(A[0])] = None
+        if noSolutionCheck:
+            continue
+        if identityMatrixCheck:
+            currentColSolution = [augmented[r][col] for r in range(len(A))]
+            solutionsDict[col-len(A[0])] = currentColSolution
+            if len(B[0]) > 1:
+                print(f"The solution for A with column {col} of B is {currentColSolution}.")
+            else:
+                print(f"The solution is {currentColSolution}.")
+            continue
+        else:
+            solutionsDict[col-len(A[0])] = "Infinite"
+            if len(B[0])>1:
+                print(f"Infinite solutions for A with column {col} of B.")
+            else:
+                print("Infinite solutions.")
+            continue
+    if len(B[0]) > 1:
+        print("The following dictionary maps the solution of the columns of B with A.")
+        print(solutionsDict)
 
-print(zeroMatrix)
